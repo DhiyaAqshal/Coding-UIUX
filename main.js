a3db092a15495a11b53a6682ae708015c23c4a0e
DhiyaAqshal <dhiya.aqshal@student.pradita.ac.id>
2024-05-27 14:15:29 +0700

    Yayakcode

---
+++ main.js
@@ -0,0 +1,14 @@
+function changeImage(x, image) {
+  if (x == 1) {
+    image.src = "Assets/foto-section4/Ayam Gepuk.png";
+  }
+  if (x == 2) {
+    image.src = "Assets/foto-section4/Ayam Santai.png";
+  }
+  if (x == 3) {
+    image.src = "Assets/foto-sectionseb2/Driver hover.png";
+  }
+  if (x == 4) {
+    image.src = "Assets/foto-sectionseb2/Driver.png";
+  }
+}
